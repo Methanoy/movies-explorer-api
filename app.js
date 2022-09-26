1dd4375ee0cb4c31dde474710f92ae878d46aa86
Aleksey Gavrikov <a.e.gavrikoff@gmail.com>
2022-09-26 21:45:11 +0300

    feat: add routes to app.js

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 /* пакетные модули */
 const express = require('express');
 const mongoose = require('mongoose');
+const userRouter = require('./routes/user');
+const moviesRouter = require('./routes/movie');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/moviesdb');
+
+app.use('/user', userRouter);
+app.use('/movies', moviesRouter);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
