f8f26e4504c389a507bcc6599068100a301c9e76
Aleksey Gavrikov <a.e.gavrikoff@gmail.com>
2022-09-26 21:21:32 +0300

    feat: add movie controllers

---
+++ controllers/movie.js
@@ -0,0 +1,75 @@
+const Movie = require('../models/movie');
+
+const getAllMovies = (req, res, next) => {
+  Movie
+    .find({})
+    .then((movies) => res.status(200).send(movies))
+    .catch(next);
+};
+
+const createMovie = (req, res, next) => {
+  const {
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailer,
+    nameRU,
+    nameEN,
+    thumbnail,
+    movieId,
+  } = req.body;
+  Movie
+    .create({
+      country,
+      director,
+      duration,
+      year,
+      description,
+      image,
+      trailer,
+      nameRU,
+      nameEN,
+      thumbnail,
+      movieId,
+      owner: req.user._id,
+    })
+    .then((movie) => res.status(201).send(movie))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequestError('Переданы некорректные данные при создании фильма.'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+const deleteMovie = (req, res, next) => {
+  const { id } = req.params;
+  Movie
+    .findById(id)
+    .then((movie) => {
+      if (!movie) {
+        throw new NotFoundError('Карточка с указанным _id не найдена.');
+      } else if (movie.owner.toString() !== req.user._id) {
+        throw new ForbiddenError('Вы не можете удалить чужую карточку.');
+      } else {
+        Movie
+          .findByIdAndRemove(id)
+          .then((movieToDelete) => {
+            res.send({ movieToDelete });
+          });
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new BadRequestError('Переданы некорректные данные при удалении карточки.'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+module.exports = { getAllMovies, deleteMovie, createMovie };
