89caaa79fdbb2cd9026b42c5d41e984dc9588dac
Aleksey Gavrikov <a.e.gavrikoff@gmail.com>
2022-09-27 22:25:34 +0300

    feat: add auth mdlwr

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const userRouter = require('./routes/user');
 const moviesRouter = require('./routes/movie');
+const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 
@@ -10,8 +11,8 @@ const app = express();
 
 mongoose.connect('mongodb://localhost:27017/moviesdb');
 
-app.use('/user', userRouter);
-app.use('/movies', moviesRouter);
+app.use('/user', auth, userRouter);
+app.use('/movies', auth, moviesRouter);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
