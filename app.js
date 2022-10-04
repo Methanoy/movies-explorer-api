14eed5cc369a0eadbb56d806efcfc89d21994387
Aleksey Gavrikov <a.e.gavrikoff@gmail.com>
2022-10-04 19:41:13 +0300

    refactor: place rateLimiter in rateLimiter.js

--- app.js
+++ app.js
@@ -4,31 +4,26 @@ const express = require('express');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
-const rateLimit = require('express-rate-limit');
 const cookieParser = require('cookie-parser');
 /* роутеры */
 const router = require('./routes/index');
 /* миддлвары */
 const cors = require('./middlewares/cors');
 const errorHandler = require('./middlewares/errorHandler');
+const rateLimiter = require('./middlewares/rateLimiter');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-});
-app.use(limiter);
-
 mongoose.connect('mongodb://localhost:27017/moviesdb');
 
+app.use(requestLogger);
+app.use(rateLimiter);
 app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
-app.use(requestLogger);
 app.use(cors);
 
 app.use(router);
